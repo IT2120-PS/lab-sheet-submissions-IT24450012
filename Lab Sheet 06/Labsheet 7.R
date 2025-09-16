b3ace193ff378c5a5078d31a7d82343d790337d9
IT24450012 <it24450012@my.sliit.lk>
2025-09-16 21:33:08 +0530

    Lab 6

---
+++ Lab Sheet 06/Labsheet 7.R
@@ -0,0 +1,14 @@
+setwd("C:\\Users\\ramod\\OneDrive\\Desktop\\Lab  7")
+
+#1
+punif(25, min=0, max=40, lower.tail=TRUE) - punif(10, min=0, max=40, lower.tail=TRUE)
+
+#2
+pexp(2, rate=1/3, lower.tail=TRUE)
+
+#3
+#i
+1-pnorm(130, mean=100, sd=15)
+
+#ii
+qnorm(0.95, mean=100, sd=15)
